50291fb59b2294980c0acb1cfb612f0ded3ccdfa
therightpathy <p_razmussen@hotmail.com>
2019-08-28 11:34:01 +0200

    Initial commit

---
+++ Student.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrushingUpUnitTesting
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+
+    public class Student
+    {
+        private string _name;
+        private string _address;
+        private int _semester;
+        private Gender _gender;
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                CheckName(value);
+                _name = value;
+            }
+        }
+
+        public string Address
+        {
+            get => _address;
+            set
+            {
+                CheckAddress(value);
+                _address = value;
+            }
+        }
+
+        public int Semester
+        {
+            get => _semester;
+            set
+            {
+                CheckSemester(value);
+                _semester = value;
+            }
+        }
+
+        public Gender Gender
+
+        {
+            get => _gender;
+            set => _gender = value;
+        }
+
+        public Student(string name, string address, int semester, Gender gender)
+        {
+            CheckName(name);
+            CheckAddress(address);
+            CheckSemester(semester);
+            Name = name;
+            Address = address;
+            Semester = semester;
+            Gender = gender;
+        }
+
+        private static bool CheckName(string name)
+        {
+            if (name.Length < 2)
+            {
+                throw new ArgumentException("Name is shorter than two characters");
+            }
+
+            return true;
+        }
+
+        private static void CheckAddress(string address)
+        {
+            if (string.IsNullOrWhiteSpace(address))
+            {
+                throw new ArgumentException("Address is null or empty");
+            }
+        }
+
+        public static void CheckSemester(int semester)
+        {
+            if (semester < 1)
+            {
+                throw new ArgumentOutOfRangeException("semester", semester, "Semester must be between 1-8");
+            }
+
+            if (semester > 8)
+            {
+                throw new ArgumentOutOfRangeException("semester", semester, "Semester must be between 1-8");
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Student({0}, {1}, {2})", Name, Address, Semester);
+        }
+
+    }
+}
